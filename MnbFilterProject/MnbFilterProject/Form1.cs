85e30536fd09353ed41be88c80314a7612cb0320
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-12-07 17:03:07 +0100

    datagridview mukodik, kilistazza az arfolyamot

--- MnbFilterProject/MnbFilterProject/Form1.cs
+++ MnbFilterProject/MnbFilterProject/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MnbFilterProject.MnbServiceReference;
+﻿using MnbFilterProject.Entities;
+using MnbFilterProject.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,15 +9,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MnbFilterProject
 {
     public partial class Form1 : Form
     {
+        BindingList<Arfolyam> Arfolyamok=new BindingList<Arfolyam>();
         public Form1()
         {
             InitializeComponent();
             CreateWebService();
+            dataGridView1.DataSource = Arfolyamok;
         }
 
         private void CreateWebService()
@@ -26,13 +30,37 @@ namespace MnbFilterProject
 
             var beertek = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2019-01-01",
-                endDate = "2020-01-01"
+                currencyNames = "DKK",
+                startDate = "2019-12-31",
+                endDate = "2020-12-31"
             };
             var visszaertek = webService.GetExchangeRates(beertek);
             var vegertek = visszaertek.GetExchangeRatesResult;
             //vegertek string!!!
+
+            var xml = new XmlDocument();
+            xml.LoadXml(vegertek);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var jelenarfolyam = new Arfolyam();
+                Arfolyamok.Add(jelenarfolyam);
+
+                jelenarfolyam.dátum = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+
+                jelenarfolyam.valuta = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    jelenarfolyam.érték = value / unit;
+                {
+
+                }
+
+
+            }
         }
     }
 }
