2b321a0e403c325eafffcf64584950734f813030
Dániel Horváth <daniel.horvath4@stud.uni-corvinus.hu>
2020-12-07 15:20:47 +0100

    Rendezés+Árfolyam osztály

---
+++ MnbFilterProject/MnbFilterProject/Entities/Arfolyam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnbFilterProject.Entities
+{
+    class Arfolyam
+    {
+        public DateTime dátum { get; set; }
+        public string valuta { get; set; }
+        public decimal érték { get; set; }
+
+    }
+}
--- MnbFilterProject/MnbFilterProject/Form1.cs
+++ MnbFilterProject/MnbFilterProject/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MnbFilterProject.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace MnbFilterProject
         public Form1()
         {
             InitializeComponent();
+            CreateWebService();
+        }
+
+        private void CreateWebService()
+        {
+            var webService = new MNBArfolyamServiceSoapClient();
+            //MNBArfolyamServiceSoapClient osztály példányosítása
+
+            var beertek = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2019-01-01",
+                endDate = "2020-01-01"
+            };
+            var visszaertek = webService.GetExchangeRates(beertek);
+            var vegertek = visszaertek.GetExchangeRatesResult;
+            //vegertek string!!!
         }
     }
 }
